d20f1f41b3b0695a4b1d92fa46944a45048b271d
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-06-04 20:37:58 +0530

    Js

--- JavaScript/practice_que1/script.js
+++ JavaScript/practice_que1/script.js
@@ -328,4 +328,24 @@
 //     console.log("Invalid");
 // }
 
-            
+
+// ## Ternary Operator
+
+// 1. Check whether a number is even or odd using ternary operator.
+// 2. Check whether age is above 18 using ternary operator.
+// 3. Find the greater number between two values using ternary operator.
+
+
+// var num = 7; 
+// var result = (num % 2 === 0) ? "Even" : "Odd";
+// console.log(result); // "Odd"
+
+// var age = 20;
+// var eligibility = (age > 18) ? "Eligible to vote" : "Not eligible to vote";
+// console.log(eligibility); // "Eligible to vote"
+
+// var a = 10;
+// var b = 20;
+// var greater = (a > b) ? a : b;
+// console.log(greater); // 20
+
