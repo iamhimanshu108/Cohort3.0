4c62d4bb360bfc20dbcf955920da0e4476d0c74a
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-06-06 21:35:51 +0530

    p

---
+++ JavaScript/livepractice1/script.js
@@ -0,0 +1,52 @@
+
+// ARRAY
+// Q1 fIND THE PRICES GREATER THAN 25
+
+let prices = [10, 20, 30, 40, 50,100,2000,300,522,545,2562,];
+
+// for(let i = 0; i < prices.length; i++){
+//     if(prices[i] > 25){
+//         console.log(prices[i]);
+//     }
+// }
+
+
+// let premiumarr= prices.filter((prices) => prices >300)
+// console.log(premiumarr)
+
+// Q2   FIND average
+
+let marks = [80,90,70,85,95];
+
+// let sum =0;
+
+// for(let i = 0; i < marks.length; i++){
+//     sum += marks[i];
+// }
+
+// let average = sum / marks.length;
+
+// console.log(average);
+
+
+// let total =marks.reduce((acc,currentValue) =>{
+//     return acc+currentValue
+// },0)
+
+// console.log(total)
+
+// const average= total/marks.length
+
+// console.log(average)
+
+
+// Q3 Most Frequent Number
+
+let number = [1,2,3,2,4,2,5,1,1,1]
+
+let count = {}
+for(let  i = 0; i <number.length; i++){
+    let num = number[i];
+    if(count[num])
+}
+
